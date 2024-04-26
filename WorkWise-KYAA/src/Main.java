33f6d945d2e5144fdec8d7130b5a4443757f1a4e
kyaagoggins <116021064+kyaagoggins@users.noreply.github.com>
2024-04-26 17:05:32 -0400

    Add files via upload

---
+++ WorkWise-KYAA/src/Main.java
@@ -0,0 +1,125 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //create account page 
+        JFrame create = new JFrame("WebWise Create Account");
+        create.setSize(500,500);
+        create.setLayout(null);
+
+        JLabel createAccount = new JLabel("Create Account");
+        createAccount.setBounds(50,50,300,30);
+         JLabel userLabel = new JLabel("Username:");
+        userLabel.setBounds(50,100,90,30);
+        JTextField createUsername = new JTextField();
+        createUsername.setBounds(175,100,200,30);
+        JLabel passLabel = new JLabel("Password:");
+        passLabel.setBounds(50,150,90,30);
+        JTextField createPassword = new JPasswordField();
+        createPassword.setBounds(175,150,200,30);
+        JLabel verifyLabel = new JLabel("Verify Password:");
+        verifyLabel.setBounds(50,200,150,30);
+        JTextField verify = new JPasswordField();
+        verify.setBounds(175,200,200,30);
+
+        //if username invalid errorUsername.setVisible(true) or create.add(errorUsername)
+        JLabel errorUsername = new JLabel("Username already exists please try again");
+        errorUsername.setBounds(50,250,300,30);
+        //same for errorPassword
+        JLabel errorPassword = new JLabel("Password must be longer than 5 characters");
+        errorPassword.setBounds(50,300,300,30);
+        errorPassword.setBackground(Color.red);
+
+        create.add(createAccount); 
+        create.add(userLabel); create.add(createUsername);
+        create.add(passLabel); create.add(createPassword);
+        create.add(verifyLabel); create.add(verify);
+        create.setVisible(true);
+
+        //create login page 
+        //once account is created, create.setVisible(false), login.setVisible(true)
+        JFrame login = new JFrame("WebWise Login Page");
+        login.setSize(500,500);
+        login.setLayout(null);
+         JLabel loginLabel = new JLabel("Login:");
+        loginLabel.setBounds(50,50,300,30);
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setBounds(50,100,90,30);
+        JTextField username = new JTextField();
+        username.setBounds(150,100,200,30);
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(50,150,90,30);
+        JPasswordField password = new JPasswordField();
+        password.setBounds(150,150,200,30);
+
+        //show based on if login is verified login.add(success) or login.add(error)
+        JLabel success = new JLabel("Success :D");
+        success.setBounds(50,200,150,30);
+        JLabel error = new JLabel("Bad :(");
+        error.setBounds(50,200,150,30);
+        
+        login.add(loginLabel);
+        login.add(usernameLabel); login.add(username);
+        login.add(passwordLabel); login.add(password);
+        login.setVisible(true);
+
+        //homepage
+        //after logged in, bring user to homepage 
+        //maybe add home, reviews, about us, and contact buttons and pages??
+        JFrame homepage = new JFrame("WebWise HomePage");
+        homepage.setSize(750,500);
+        homepage.setLayout(null);
+        JLabel workwise = new JLabel("WORKWISE");
+        workwise.setFont(new Font("Open Sans", Font.BOLD, 22));
+        workwise.setForeground(Color.RED);
+        workwise.setBounds(300,50,300,30);
+        String companies[] = {"Search","Google", "Amazon", "Meta"};
+        JComboBox searchBar = new JComboBox<>(companies);
+        searchBar.setBounds(200,100,300,30);
+        JLabel jobImageLabel = new JLabel();
+        ImageIcon jobImage = new ImageIcon("src/Images/WorkwiseJobImage.jpeg");
+        jobImageLabel.setBounds(150,300,200,200);
+        jobImageLabel.setIcon(jobImage);
+
+        homepage.add(workwise);
+        homepage.add(searchBar);
+        homepage.add(jobImageLabel);
+        homepage.setVisible(true);
+
+
+
+
+
+
+        Scanner sc = new Scanner(System.in);
+        AccountHandler acc = new AccountHandler();
+
+        // this is setup to test creating acc and logging in
+        // create user, then log in attempt, then display all stored usernames and pass
+        for (int i = 0; i < 5; i++) {
+            System.out.println("\n\ncreate account");
+            acc.createAccount();
+
+
+            System.out.println("\n\n\nlogin:");
+            System.out.println("enter username: ");
+            String user = sc.nextLine();
+            System.out.println("enter password: ");
+            String pass = sc.nextLine();
+            if (acc.loginAttempt(user, pass)) {
+                System.out.println("sucess :D");
+            } else {
+                System.out.println(" bad :(");
+            }
+
+        acc.dangerMethod();
+
+        }
+
+        
+    }
+}
