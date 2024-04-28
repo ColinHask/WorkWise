6ad76d27de6e0fc4e36d4158c8277af0d657f916
kyaagoggins <116021064+kyaagoggins@users.noreply.github.com>
2024-04-28 18:55:15 -0400

    Add files via upload
    comments added

--- src/Main.java
+++ src/Main.java
@@ -10,61 +10,83 @@ public class Main {
     public static void main(String[] args) {
 
         //create account page 
+        //this creates and sets the frame of the create account page
         JFrame create = new JFrame("WebWise Create Account");
         create.setSize(500,500);
         create.setLayout(null);
 
+        //the label for create account and its placement
         JLabel createAccount = new JLabel("Create Account");
         createAccount.setBounds(50,50,300,30);
+
+        //the label and its placement for the username input field
          JLabel userLabel = new JLabel("Username:");
         userLabel.setBounds(50,100,90,30);
+        //username input field
         JTextField createUsername = new JTextField();
         createUsername.setBounds(175,100,200,30);
+        //label and placement for password field
         JLabel passLabel = new JLabel("Password:");
         passLabel.setBounds(50,150,90,30);
+        //creating password input field and its placement
         JTextField createPassword = new JPasswordField();
         createPassword.setBounds(175,150,200,30);
+        //label and placement for verify password field
         JLabel verifyLabel = new JLabel("Verify Password:");
         verifyLabel.setBounds(50,200,150,30);
+        //verify password input field and placement
         JTextField verify = new JPasswordField();
         verify.setBounds(175,200,200,30);
 
+        //text label for error username if it already exists
         //if username invalid errorUsername.setVisible(true) or create.add(errorUsername)
         JLabel errorUsername = new JLabel("Username already exists please try again");
         errorUsername.setBounds(50,250,300,30);
-        //same for errorPassword
+        //text label for error password if password is not long enough
+        //if the password is not valid then errorPassword.setVisible or create.add(errorPassword)
         JLabel errorPassword = new JLabel("Password must be longer than 5 characters");
         errorPassword.setBounds(50,300,300,30);
         errorPassword.setBackground(Color.red);
 
+        //adds all fields created to the create account page/frame 
         create.add(createAccount); 
         create.add(userLabel); create.add(createUsername);
         create.add(passLabel); create.add(createPassword);
         create.add(verifyLabel); create.add(verify);
+        //sets the create page to visible
         create.setVisible(true);
 
         //create login page 
         //once account is created, create.setVisible(false), login.setVisible(true)
+        //login page and set size and layout
         JFrame login = new JFrame("WebWise Login Page");
         login.setSize(500,500);
         login.setLayout(null);
+        //login label for page
          JLabel loginLabel = new JLabel("Login:");
         loginLabel.setBounds(50,50,300,30);
+        //username label and placement
         JLabel usernameLabel = new JLabel("Username:");
         usernameLabel.setBounds(50,100,90,30);
+        //username text field creation and placement
         JTextField username = new JTextField();
         username.setBounds(150,100,200,30);
+        //password label and placement
         JLabel passwordLabel = new JLabel("Password:");
         passwordLabel.setBounds(50,150,90,30);
+        //password text field creation and placement 
         JPasswordField password = new JPasswordField();
         password.setBounds(150,150,200,30);
 
         //show based on if login is verified login.add(success) or login.add(error)
+        //success label message and placement 
         JLabel success = new JLabel("Success :D");
         success.setBounds(50,200,150,30);
+        //error label message and placement 
         JLabel error = new JLabel("Bad :(");
         error.setBounds(50,200,150,30);
         
+        //adds created fields to the login page/frame
         login.add(loginLabel);
         login.add(usernameLabel); login.add(username);
         login.add(passwordLabel); login.add(password);
